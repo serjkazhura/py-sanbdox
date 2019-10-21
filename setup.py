9f6399d30d75cfcbf05268b95f31e07f161e2b54
sergey kazhura <gray.serj@gmail.com>
2019-10-20 20:52:35 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+  name='sandbox-py',
+  version='1.0.0',
+  py_modules=['words', 'comprehensions'],
+
+  # metadata
+  author='Sergei K',
+  description='Python playground',
+  license='Public domain',
+  keywords='examples'
+)
